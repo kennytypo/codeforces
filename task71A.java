e014d8643f56c949a84fd117a703e70be41b486b
Anton Gurovskii <john.sss.16@yandex.ru>
2023-05-18 18:52:17 +0300

    task71A

--- task71A.java
+++ task71A.java
@@ -1,22 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class task71A {
-    public static void tooLongStrings(){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
         ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        int n = sc.nextInt();
+        for (int i = 0; i <= n; i++) {
             list.add(sc.nextLine());
         }
 
         for (int i = 0; i < list.size(); i++) {
             String str = list.get(i);
             if (str.length() > 10){
-                StringBuilder builder = new StringBuilder(str);
+                String newStr = "";
+                StringBuilder builder = new StringBuilder(newStr);
                 builder.append(str.charAt(0));
                 builder.append(str.length()-2);
                 builder.append(str.charAt(str.length()-1));
