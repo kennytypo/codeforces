a6f9de03116c4a4c1949fd18e0392986f5f6edbe
Anton <112724184+kennytypo@users.noreply.github.com>
2023-05-18 18:58:28 +0300

    Add files via upload

---
+++ task282A.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class task282A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            list.add(sc.nextLine());
+        }
+        int result = 0;
+        for (int i = 1; i < list.size(); i++) {
+            String str = list.get(i);
+            if (str.charAt(0) == 'X'){
+                if (str.charAt(1) == '+'){
+                    result++;
+                } else if (str.charAt(1) == '-') {
+                    result--;
+                }
+            } else if (str.charAt(2) == 'X') {
+                if (str.charAt(0) == '+'){
+                    ++result;
+                } else if (str.charAt(0) == '-') {
+                    --result;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
